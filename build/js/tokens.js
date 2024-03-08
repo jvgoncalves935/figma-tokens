8c98f915d0bf0ab96d36c283cd6fdf18655349f0
João Vítor Gonçalves <jvgoncalves935@hotmail.com>
2024-03-08 08:43:21 -0300

    First Commit

---
+++ build/js/tokens.js
@@ -0,0 +1,14 @@
+/**
+ * Do not edit directly
+ * Generated on Thu, 07 Mar 2024 14:09:35 GMT
+ */
+
+module.exports = {
+  "ColorWhite": "#ffffff",
+  "ColorGray200": "#c1c1c4",
+  "ColorGray500": "#50505c",
+  "ColorGray800": "#1c1c1f",
+  "ColorGray900": "#121214",
+  "ColorPurple500": "#8257e6",
+  "EffectShadowMd": {"shadowType":"dropShadow","radius":4,"color":"#00000040","offsetX":0,"offsetY":4,"spread":0}
+};
